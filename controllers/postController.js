f180bd0ff3585548c677e7b897fa76d65b3d7197
enrico-antonini1 <enry1997@me.com>
2026-01-13 02:03:23 +0100

    feat: update

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,42 +1,48 @@
 import post from "../assets/post.js";
 
-function index(req,res) {
+function index(req, res) {
   res.json(post);
 }
 
-function show(req,res) {
-    const id = parseInt(req.params.id)
-    const actualPost = post.find((p) => p.id === id)
-    res.json(actualPost)
+function show(req, res) {
+  const id = parseInt(req.params.id);
+  const actualPost = post.find((p) => p.id === id);
+  res.json(actualPost);
 }
 
-function store(req,res) {
-    const data = req.body;
+function store(req, res) {
+  const data = req.body;
 
-    const newId = post[post.length-1].id + 1;
-    
-    const newPost = {
-        id: newId,
-        ...data,
-    }
+  const newId = post[post.length - 1].id + 1;
 
-    post.push(newPost);
-    res.status(201);
-    res.json(newPost);
+  const newPost = {
+    id: newId,
+    ...data,
+  };
+
+  post.push(newPost);
+  res.status(201);
+  res.json(newPost);
 }
 
-function update(req,res) {
-    const id = parseInt(req.params.id)
-    const actualPost = post.find((p) => p.id === id)
+function update(req, res) {
+  const id = parseInt(req.params.id);
+  const actualPost = post.find((p) => p.id === id);
+  const data = req.body;
+
+  actualPost.titolo = data.titolo;
+  actualPost.contenuto = data.contenuto;
+  actualPost.immagine = data.immagine;
+  actualPost.tags = data.tags;
+
+  res.json(actualPost)
 }
 
-function modify(req,res) {
-    
-}
+function modify(req, res) {}
 
-function destroy(req,res) {
-    const id = parseInt(req.params.id)
-    const actualPost = post.find((p) => p.id === id)
+function destroy(req, res) {
+  const id = parseInt(req.params.id);
+  const actualPost = post.find((p) => p.id === id);
 }
 
 export default {
